d74c3be780346bb69d3c36d3c52444b185f86c8e
Joshua Haddock <joshhaddock88@gmail.com>
2021-07-13 21:06:06 -0700

    Complete Lab with tests

--- Lab02/Lab02/Program.cs
+++ Lab02/Lab02/Program.cs
@@ -2,7 +2,7 @@
 
 namespace Lab02
 {
-    class Program
+    public class Program
     {
 
         public static decimal Balance = 100.00M; 
@@ -15,118 +15,71 @@ namespace Lab02
 
         public static void UserInterFace()
         {
-            Console.WriteLine("Welcome! Please type the number associated with the action you wish to perform:");
-            Console.WriteLine("1. View Balance");
-            Console.WriteLine("2. Withdraw");
-            Console.WriteLine("3. Deposit");
-            Console.WriteLine("4. Exit");
+            Console.WriteLine("Welcome! Please type the number associated with the action you wish to perform:" +
+                "\n1. View Balance " +
+                "\n2. Withdraw " +
+                "\n3. Deposit " +
+                "\n4. Exit");
             Console.Write("Please choose an action to perform: ");
             string menuInput = Console.ReadLine();
+            while (menuInput != "1" && menuInput != "2" && menuInput != "3" && menuInput != "4")
+            {
+                Console.Write("Input invalid. Please try again: ");
+                menuInput = Console.ReadLine();
+            }
             //------------------------------------------View Balance----------------------------------------
             if(menuInput == "1")
             {
+                Console.Clear();
                 Console.WriteLine("View Balance");
                 Console.WriteLine($"Current Balance: {ViewBalance()}");
-                Console.Write("Press 4 to return to main menu: ");
-                string balanceInput = Console.ReadLine();
-                while (balanceInput != "4")
-                {
-                    Console.Write("Press 4 to return to main menu: ");
-                    balanceInput = Console.ReadLine();
-                }
-                if (balanceInput == "4")
-                {
-                    Console.Clear();
-                    UserInterFace();
-                }
+                ReturnHome();
             }
             //---------------------------------------Withdraw------------------------------------------------
             if(menuInput == "2")
             {
+                Console.Clear();
                 Console.WriteLine("Withdraw Funds");
-                Console.WriteLine($"Current Balance: {Balance}");
-                Console.Write("Press 1 to withdraw or 4 to exit to main menu: ");
-                string withdrawInput = Console.ReadLine();
-                while(withdrawInput != "1" && withdrawInput != "4")
-                {
-                    Console.Write("Invalid option, please press 1 or 4: ");
-                    withdrawInput = Console.ReadLine();
-                }
-                if(withdrawInput == "4")
-                {
-                    Console.Clear();
-                    UserInterFace();
-                }
-                if (withdrawInput == "1")
+                try
                 {
                     Console.Write("Amount: ");
                     decimal withdrawalAmount = Convert.ToDecimal(Console.ReadLine());
-                    while (withdrawalAmount > Balance || withdrawalAmount < 0)
-                    {
-                        Console.WriteLine("Error: insufficient funds");
-                        Console.Write("Amount: ");
-                        withdrawalAmount = Convert.ToDecimal(Console.ReadLine());
-                    }
                     Withdraw(withdrawalAmount);
-                    Console.WriteLine($"{withdrawalAmount} withdrawn.");
-                    Console.WriteLine($"Press 4 to return to menu");
-                    withdrawInput = Console.ReadLine();
-                    while(withdrawInput != "4")
-                    {
-                        Console.WriteLine($"Press 4 to return to menu");
-                        withdrawInput = Console.ReadLine();
-                    }
-                    Console.Clear();
-                    UserInterFace();
                 }
+                catch(FormatException)
+                {
+                    Console.WriteLine("Invalid format");
+                    ReturnHome();
+                }
+                ReturnHome();
             }
             //------------------------------------------------------Deposit-------------------------------------------
             if(menuInput == "3")
             {
+                Console.Clear();
                 Console.WriteLine("Deposit Funds");
-                Console.WriteLine($"Current Balance: {Balance}");
-                Console.Write("Press 1 to deposit or 4 to exit to main menu: ");
-                string depositInput = Console.ReadLine();
-                while (depositInput != "1" && depositInput != "4")
-                {
-                    Console.Write("Invalid option, please press 1 or 4: ");
-                    depositInput = Console.ReadLine();
-                }
-                if (depositInput == "4")
-                {
-                    Console.Clear();
-                    UserInterFace();
-                }
-                if (depositInput == "1")
+                try
                 {
                     Console.Write("Amount: ");
                     decimal depositAmount = Convert.ToDecimal(Console.ReadLine());
-                    while (depositAmount < 0)
-                    {
-                        Console.WriteLine("Error: incorrect value");
-                        Console.Write("Amount: ");
-                        depositAmount = Convert.ToDecimal(Console.ReadLine());
-                    }
                     Deposit(depositAmount);
-                    Console.WriteLine($"{depositAmount} deposited.");
-                    Console.WriteLine($"Press 4 to return to menu");
-                    depositInput = Console.ReadLine();
-                    while (depositInput != "4")
-                    {
-                        Console.WriteLine($"Press 4 to return to menu");
-                        depositInput = Console.ReadLine();
-                    }
-                    Console.Clear();
-                    UserInterFace();
                 }
+                catch(FormatException)
+                {
+                    Console.WriteLine("Invalid format");
+                    ReturnHome();
+                }
+                ReturnHome();
             }
             //--------------------------------------------------------Exit-------------------------------------
             if(menuInput == "4")
             {
+                Console.Clear();
                 return;
             }
         }
 
+        //--------------------------------------------------------Other Methods-------------------------------
         public static decimal ViewBalance()
         {
             return Balance;
@@ -134,23 +87,45 @@ namespace Lab02
 
         public static decimal Withdraw(decimal number)
         {
+            if (number > Balance)
+            {
+                Console.WriteLine("Error: insufficient funds");
+                return -1;
+            }
+            if (number < 0)
+            {
+                Console.WriteLine("Error: incorrect value");
+                return -1;
+            }
             Balance -= number;
+            Console.WriteLine($"{number} withdrawn.");
+
             return Balance;
         }
 
         public static decimal Deposit(decimal number)
         {
+            if (number < 0)
+            {
+                Console.WriteLine("Error: incorrect value");
+                return -1;
+            }
             Balance += number;
             return Balance;
+        }
+
+        public static void ReturnHome()
+        {
+            Console.WriteLine($"Press 1 to return");
+            string userInput = Console.ReadLine();
+            while (userInput != "1")
+            {
+                Console.WriteLine($"Press 1 to return");
+                userInput = Console.ReadLine();
+            }
+            Console.Clear();
+            UserInterFace();
         }
     }
 }
 
-
-//1. Create UserInterface Method
-//  a. UserInterFace Welcome Message
-//  b. UserInterface options
-//      i. View Balance
-//      ii. Withdraw
-//      iii. Deposit
-//      iv. Exit
