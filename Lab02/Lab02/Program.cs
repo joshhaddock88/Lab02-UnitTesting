e659d5feef181593268d3282d756a4f500f8cdcd
Joshua Haddock <joshhaddock88@gmail.com>
2021-07-13 19:08:47 -0700

    Working front end. Need refactoring

--- Lab02/Lab02/Program.cs
+++ Lab02/Lab02/Program.cs
@@ -20,12 +20,108 @@ namespace Lab02
             Console.WriteLine("2. Withdraw");
             Console.WriteLine("3. Deposit");
             Console.WriteLine("4. Exit");
-            string userInput = Console.ReadLine();
-            if(userInput == "1")
+            Console.Write("Please choose an action to perform: ");
+            string menuInput = Console.ReadLine();
+            //------------------------------------------View Balance----------------------------------------
+            if(menuInput == "1")
             {
-                ViewBalance();
+                Console.WriteLine("View Balance");
+                Console.WriteLine($"Current Balance: {ViewBalance()}");
+                Console.Write("Press 4 to return to main menu: ");
+                string balanceInput = Console.ReadLine();
+                while (balanceInput != "4")
+                {
+                    Console.Write("Press 4 to return to main menu: ");
+                    balanceInput = Console.ReadLine();
+                }
+                if (balanceInput == "4")
+                {
+                    Console.Clear();
+                    UserInterFace();
+                }
             }
-            if(userInput == "4")
+            //---------------------------------------Withdraw------------------------------------------------
+            if(menuInput == "2")
+            {
+                Console.WriteLine("Withdraw Funds");
+                Console.WriteLine($"Current Balance: {Balance}");
+                Console.Write("Press 1 to withdraw or 4 to exit to main menu: ");
+                string withdrawInput = Console.ReadLine();
+                while(withdrawInput != "1" && withdrawInput != "4")
+                {
+                    Console.Write("Invalid option, please press 1 or 4: ");
+                    withdrawInput = Console.ReadLine();
+                }
+                if(withdrawInput == "4")
+                {
+                    Console.Clear();
+                    UserInterFace();
+                }
+                if (withdrawInput == "1")
+                {
+                    Console.Write("Amount: ");
+                    decimal withdrawalAmount = Convert.ToDecimal(Console.ReadLine());
+                    while (withdrawalAmount > Balance || withdrawalAmount < 0)
+                    {
+                        Console.WriteLine("Error: insufficient funds");
+                        Console.Write("Amount: ");
+                        withdrawalAmount = Convert.ToDecimal(Console.ReadLine());
+                    }
+                    Withdraw(withdrawalAmount);
+                    Console.WriteLine($"{withdrawalAmount} withdrawn.");
+                    Console.WriteLine($"Press 4 to return to menu");
+                    withdrawInput = Console.ReadLine();
+                    while(withdrawInput != "4")
+                    {
+                        Console.WriteLine($"Press 4 to return to menu");
+                        withdrawInput = Console.ReadLine();
+                    }
+                    Console.Clear();
+                    UserInterFace();
+                }
+            }
+            //------------------------------------------------------Deposit-------------------------------------------
+            if(menuInput == "3")
+            {
+                Console.WriteLine("Deposit Funds");
+                Console.WriteLine($"Current Balance: {Balance}");
+                Console.Write("Press 1 to deposit or 4 to exit to main menu: ");
+                string depositInput = Console.ReadLine();
+                while (depositInput != "1" && depositInput != "4")
+                {
+                    Console.Write("Invalid option, please press 1 or 4: ");
+                    depositInput = Console.ReadLine();
+                }
+                if (depositInput == "4")
+                {
+                    Console.Clear();
+                    UserInterFace();
+                }
+                if (depositInput == "1")
+                {
+                    Console.Write("Amount: ");
+                    decimal depositAmount = Convert.ToDecimal(Console.ReadLine());
+                    while (depositAmount < 0)
+                    {
+                        Console.WriteLine("Error: incorrect value");
+                        Console.Write("Amount: ");
+                        depositAmount = Convert.ToDecimal(Console.ReadLine());
+                    }
+                    Deposit(depositAmount);
+                    Console.WriteLine($"{depositAmount} deposited.");
+                    Console.WriteLine($"Press 4 to return to menu");
+                    depositInput = Console.ReadLine();
+                    while (depositInput != "4")
+                    {
+                        Console.WriteLine($"Press 4 to return to menu");
+                        depositInput = Console.ReadLine();
+                    }
+                    Console.Clear();
+                    UserInterFace();
+                }
+            }
+            //--------------------------------------------------------Exit-------------------------------------
+            if(menuInput == "4")
             {
                 return;
             }
@@ -33,30 +129,19 @@ namespace Lab02
 
         public static decimal ViewBalance()
         {
-            Console.WriteLine("View Balance");
-            Console.WriteLine($"Current Balance: {Balance}");
-            Console.Write("Press 4 to return to main menu");
-            string userInput = Console.ReadLine();
-            while(userInput != "4")
-            {
-                Console.Write("Press 4 to return to main menu: ");
-                userInput = Console.ReadLine();
-            }
-            if (userInput == "4")
-            {
-                UserInterFace();
-            }
             return Balance;
         }
 
-        public static decimal Withdraw()
+        public static decimal Withdraw(decimal number)
         {
-            return 1.20M;
+            Balance -= number;
+            return Balance;
         }
 
-        public static decimal Deposit()
+        public static decimal Deposit(decimal number)
         {
-            return 1.20M;
+            Balance += number;
+            return Balance;
         }
     }
 }
